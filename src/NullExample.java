633c94bf2a1fd5c8771f38c959467135f03278a6
Subeibei <1275366583@qq.com>
2015-10-13 23:02:59 +0800

    init commit

---
+++ src/NullExample.java
@@ -0,0 +1,40 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class NullExample {
+    static class Foo {
+        String str = "";
+        int num;
+
+        public Foo() {
+            this.str = null;
+            num = 5;
+        }
+
+        @Override
+        public String toString() {
+            return  "str: " + str + "\n" +
+                    "num: " + num + "\n";
+        }
+    }
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+
+        Gson gson1 = new Gson();
+        Gson gson2 = new GsonBuilder().serializeNulls().create();
+
+        // Serialization
+        System.out.println(gson1.toJson(foo));
+        System.out.println(gson2.toJson(foo));
+
+        // Deserialization
+        String str1 = "{\"num\":5}";
+        System.out.println(gson1.fromJson(str1, Foo.class));
+        System.out.println(gson2.fromJson(str1, Foo.class));
+
+        String str2 = "{\"str\":null,\"num\":5}";
+        System.out.println(gson1.fromJson(str2, Foo.class));
+        System.out.println(gson2.fromJson(str2, Foo.class));
+    }
+}
